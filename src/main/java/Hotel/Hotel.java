c6cd6594705a6f5b4af214cd259fcc65333e7384
paulinak03 <paulinakulzer03@web.de>
2025-12-03 18:13:04 +0100

    Erstellen von Attributen und Konstruktor

--- src/main/java/Hotel/Hotel.java
+++ src/main/java/Hotel/Hotel.java
@@ -2,6 +2,21 @@ package Hotel;
 
 public class Hotel {
 
+    //Attribute deklarieren:
     public String name;
-    public int zahl;
+    public String standort;
+    public int anzahlSterne;
+    public double preisProNacht;
+    public boolean luxsushotel;
+
+    //Konstruktor erstellen:
+    public Hotel(String name, String standort, int anzahlSterne, double preisProNacht) {
+        this.name = name;
+        this.standort = standort;
+        this.anzahlSterne = anzahlSterne;
+        this.preisProNacht = preisProNacht;
+
+    //Ist das Hotel ein Luxushotel, auf Grund von Preis oder Sterne:
+
+    }
 }
